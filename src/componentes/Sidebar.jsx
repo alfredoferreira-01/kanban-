ac83a196a3e09ec8e9be5df66d3de6a54b6aca82
alunoctg <alunoctg@fiern.local>
2026-08-25 11:29:45 -0300

    ajuste

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,37 +1,43 @@
-import { NavLink } from 'react-router';
-import styles from './Sidebar.module.css';
+import { NavLink } from "react-router";
+import styles from "Sidebar.module.css";
 
 function Sidebar({ logado, onLogout }) {
+  const linkClass = ({ isActive }) =>
+    isActive ? styles.link + " " + styles.ativo : styles.link;
 
-const linkClass = ({ isActive }) =>
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.logo}>
+        <h1>TaskFlow</h1>
+      </div>
 
-isActive ? styles.link + ' ' + styles.ativo : styles.link;
+      <nav className={styles.nav}>
+        {logado && (
+          <NavLink to="/kanban" className={linkClass}>
+            Kanban
+          </NavLink>
+        )}
+        {logado && (
+          <NavLink to="/Tarefa" className={linkClass}>
+            Tarefa
+          </NavLink>
+        )}
 
-return (
+        <NavLink to="/sobre" className={linkClass}>
+          Sobre
+        </NavLink>
+        <NavLink to="/Login" className={linkClass}>
+          Login
+        </NavLink>
+      </nav>
 
-<aside className={styles.sidebar}>
-
-<div className={styles.logo}><h1>TaskFlow</h1></div>
-
-<nav className={styles.nav}>
-
-
-{logado && <NavLink to='/kanban' className={linkClass}>Kanban</NavLink>}
-{logado && <NavLink to='/Tarefa' className={linkClass}>Tarefa</NavLink>}
-
-<NavLink to='/sobre' className={linkClass}>Sobre</NavLink>
-<NavLink to='/Login' className={linkClass}>Login</NavLink>
-
-</nav>
-
-
-
-{logado && (<button className={styles.btnLogout} onClick={onLogout}>Sair</button>)}
-
-</aside>
-
-);
-
+      {logado && (
+        <button className={styles.btnLogout} onClick={onLogout}>
+          Sair
+        </button>
+      )}
+    </aside>
+  );
 }
 
 export default Sidebar;
