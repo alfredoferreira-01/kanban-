02f48deaccaa9d48fa64824f250a71beb098d9dd
alfredo <alfredo.f.neto6@aluno.senai.br>
2026-08-27 10:22:34 -0300

    ajuste sidebar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router";
-import styles from "./Sidebar.module.Css";
+import styles from "./Sidebar.module.css";
 
 function Sidebar({ logado, onLogout }) {
   const linkClass = ({ isActive }) =>
